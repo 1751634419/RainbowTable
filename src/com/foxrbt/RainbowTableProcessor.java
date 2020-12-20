c353670bc9d74eda4055237604fd712cf1bc6648
William Fox <1751634419@qq.com>
2020-12-20 11:48:15 +0800

    Initial commit

--- src/com/foxrbt/RainbowTableProcessor.java
+++ src/com/foxrbt/RainbowTableProcessor.java
@@ -62,15 +62,6 @@ public class RainbowTableProcessor {
         return null;
     }
 
-    private <K, V> K getKeyByLoop(Map<K, V> map, V value) {
-        for (Map.Entry<K, V> entry : map.entrySet()) {
-            if (Objects.equals(entry.getValue(), value)) {
-                return entry.getKey();
-            }
-        }
-        return null;
-    }
-
     private <K, V> Set<K> getKeysByLoop(Map<K, V> map, V value) {
         Set<K> set = new HashSet<>();
         for (Map.Entry<K, V> entry : map.entrySet()) {
